e4e159037553440be33fcf899e531343046a799b
Stefan <epiktetov.sv@gmail.com>
2023-06-25 14:47:30 +0300

    added email filter

--- mountains/views.py
+++ mountains/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters import rest_framework
 
 from .serializers import MountainSerializer
 from .models import Mountain
@@ -7,3 +8,5 @@ from .models import Mountain
 class MountainsViewset(viewsets.ModelViewSet):
     queryset = Mountain.objects.all()
     serializer_class = MountainSerializer
+    filter_backends = [rest_framework.DjangoFilterBackend]
+    filterset_fields = ['user__email']
